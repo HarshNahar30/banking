193cb15f66786c9d1c86341bc8ae28acb60dce74
HarshNahar30 <100199057+HarshNahar30@users.noreply.github.com>
2024-07-22 18:39:41 +0530

    complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+
+  const loggedIn = { firstName : 'Harsh', lastName : 'Nahar', email : 'harshnahar2003@gmail.com'};
+
   return (
-    <div>page</div>
+    <section className='home'>
+      <div className="home-content">
+        <header className='home-header'>
+
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+
+          <TotalBalanceBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance:12333.45}, {currentBalance: 54234.56}]}
+      />
+    </section>
   )
 }
 
