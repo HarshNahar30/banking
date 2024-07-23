880f6b37b035084fc2f3a4ac9280bf3e9de395bc
HarshNahar30 <100199057+HarshNahar30@users.noreply.github.com>
2024-07-24 02:22:26 +0530

    fully implemented appwrite and sentry

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -22,13 +22,14 @@ import CustomInput from './CustomInput';
 import { authFormSchema } from '@/lib/utils';
 import { Loader2 } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-// import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
 // import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
   const router = useRouter();
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  // const loggedInUser = await getLoggedInUser()
 
   const formSchema = authFormSchema(type);
 
@@ -43,40 +44,40 @@ const AuthForm = ({ type }: { type: string }) => {
     const onSubmit = async (data: z.infer<typeof formSchema>) => {
       setIsLoading(true);
 
-    //   try {
+      try {
         
-    //     if(type === 'sign-up') {
-    //       const userData = {
-    //         firstName: data.firstName!,
-    //         lastName: data.lastName!,
-    //         address1: data.address1!,
-    //         city: data.city!,
-    //         state: data.state!,
-    //         postalCode: data.postalCode!,
-    //         dateOfBirth: data.dateOfBirth!,
-    //         ssn: data.ssn!,
-    //         email: data.email,
-    //         password: data.password
-    //       }
+        if(type === 'sign-up') {
+          // const userData = {
+          //   firstName: data.firstName!,
+          //   lastName: data.lastName!,
+          //   address1: data.address1!,
+          //   city: data.city!,
+          //   state: data.state!,
+          //   postalCode: data.postalCode!,
+          //   dateOfBirth: data.dateOfBirth!,
+          //   ssn: data.ssn!,
+          //   email: data.email,
+          //   password: data.password
+          // }
 
-    //       const newUser = await signUp(userData);
+          const newUser = await signUp(data);
 
-    //       setUser(newUser);
-    //     }
+          setUser(newUser);
+        }
 
-    //     if(type === 'sign-in') {
-    //       const response = await signIn({
-    //         email: data.email,
-    //         password: data.password,
-    //       })
+        if(type === 'sign-in') {
+          const response = await signIn({
+            email: data.email, 
+            password: data.password,
+          })
 
-    //       if(response) router.push('/')
-    //     }
-    //   } catch (error) {
-    //     console.log(error);
-    //   } finally {
-    //     setIsLoading(false);
-    //   }
+          if(response) router.push('/')
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
     }
 
   return (
